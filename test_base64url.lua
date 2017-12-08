31911d76e467f59631f8ccfc66db6aa8409840f1
林瑞潮 <linruichao@linruichaodeMacBook.local>
2017-12-08 20:19:01 +0800

    更新 base64url 测试用例

--- test_base64url.lua
+++ test_base64url.lua
@@ -1,7 +1,7 @@
 local base64url = require("base64url")
 
-assert(base64url.from_base64("TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ=") ==
-    "TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ")
+assert(base64url.from_base64("PPp27xSTfBwOpRn4/AV6gPzQSnQg+Oi80KdWfCcuAHs=") ==
+    "PPp27xSTfBwOpRn4_AV6gPzQSnQg-Oi80KdWfCcuAHs")
 
-assert(base64url.to_base64("TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ") ==
-    "TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ=")
+assert(base64url.to_base64("PPp27xSTfBwOpRn4_AV6gPzQSnQg-Oi80KdWfCcuAHs") ==
+    "PPp27xSTfBwOpRn4/AV6gPzQSnQg+Oi80KdWfCcuAHs=")
